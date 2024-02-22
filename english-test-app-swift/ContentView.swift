770c085921fb2fe386f87a23dc1c5673d0b736d9
KEN-4 <ssstututusss617@gmail.com>
2024-02-22 10:59:37 +0900

    Initial Commit

---
+++ english-test-app-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  english-test-app-swift
+//
+//  Created by 内藤広貴 on 2024/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ english-test-app-swift/english_test_app_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  english_test_app_swiftApp.swift
+//  english-test-app-swift
+//
+//  Created by 内藤広貴 on 2024/02/22.
+//
+
+import SwiftUI
+
+@main
+struct english_test_app_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
