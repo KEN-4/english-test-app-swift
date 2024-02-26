fb683555e8b4f8a5f8ae5cf31c173b44d4134910
KEN-4 <ssstututusss617@gmail.com>
2024-02-26 19:27:09 +0900

    Update ResultView

--- english-test-app-swift/ResultView.swift
+++ english-test-app-swift/ResultView.swift
@@ -17,35 +17,7 @@ struct ResultView: View {
                 ForEach(getMostNeededStudyMethods(scores: scoreModel.scores), id: \.self) { recommendation in
                     Text(recommendation)
                 }
-
-                Button("ログアウト") {
-                    try? Auth.auth().signOut()
-                    // ログインページへの遷移ロジックをここに追加
-                }
-                .padding()
-                .foregroundColor(.white)
-                .background(Color.red)
-                .cornerRadius(10)
             }
-            .padding()
-        }
-        .onAppear {
-            saveResultsToFirestore(scoreModel: scoreModel)
-        }
-    }
-}
-
-func saveResultsToFirestore(scoreModel: ScoreModel) {
-    guard let user = Auth.auth().currentUser else { return }
-    let uid = user.uid
-    let scores = scoreModel.scores
-
-    let docRef = Firestore.firestore().collection("users").document(uid)
-    docRef.updateData([
-        "result_scores": scores
-    ]) { error in
-        if let error = error {
-            print("Firestore保存エラー: \(error)")
         }
     }
 }
