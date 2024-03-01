1d1ec9a8cf14a961c20b47c19bc9d659593db241
KEN-4 <ssstututusss617@gmail.com>
2024-03-01 11:16:40 +0900

    Update ResultView

--- english-test-app-swift/ChoiceQuestionView.swift
+++ english-test-app-swift/ChoiceQuestionView.swift
@@ -3,36 +3,40 @@ import AVFoundation
 
 struct ChoiceQuestionView: View {
     @ObservedObject var viewModel: QuestionViewModel
-    
+    @State private var showingModal = false // モーダル表示のための状態変数
+
     var body: some View {
-        NavigationView {
-            VStack {
-                    if let question = viewModel.currentQuestion {
-                        Text("正しいと思う選択肢を選んでください")
-                        ForEach(question.sentences, id: \.self) { sentence in
-                            Text(sentence)
-                                .padding(.bottom, 8)
-                        }
-                        ForEach(question.choices, id: \.self) { choice in
-                            Button(choice) {
-                                viewModel.checkAnswer(choice: choice)
-                            }
-                            .disabled(viewModel.isAnswered)
-                        }
-                        if viewModel.isAnswered {
-                            Text("正解: \(question.correctAnswer)")
-                            Button("次の質問") {
-                                viewModel.goToNextQuestion()
-                            }
-                        }
-                    } else {
-                        Text("質問をロード中...")
-                    }
-                    // ResultViewへの遷移を制御するNavigationLink
-                NavigationLink(destination: ResultView(scoreModel: viewModel.scoreModel), isActive: $viewModel.showResultView) {
-                    EmptyView()
+        VStack {
+            if let question = viewModel.currentQuestion {
+                Text("正しいと思う選択肢を選んでください")
+                ForEach(question.sentences, id: \.self) { sentence in
+                    Text(sentence)
+                        .padding(.bottom, 8)
                 }
+                ForEach(question.choices, id: \.self) { choice in
+                    Button(choice) {
+                        viewModel.checkAnswer(choice: choice)
+                    }
+                    .disabled(viewModel.isAnswered)
+                }
+                if viewModel.isAnswered {
+                    Text("正解: \(question.correctAnswer)")
+                    Button("次の質問") {
+                        // モーダルを表示するために状態変数をtrueに設定
+                        showingModal = true
+                    }
+                }
+            } else {
+                Text("質問をロード中...")
             }
         }
+        // ResultViewへのモーダル遷移を設定
+        .fullScreenCover(isPresented: $showingModal) {
+            // ここでモーダルとして表示したいビューを指定
+            ResultView(scoreModel: viewModel.scoreModel)
+        }
+        .navigationTitle("Choice Question")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
+
--- english-test-app-swift/ResultView.swift
+++ english-test-app-swift/ResultView.swift
@@ -6,17 +6,14 @@ struct ResultView: View {
     var scoreModel = ScoreModel()
 
     var body: some View {
-        ScrollView {
-            VStack(spacing: 20) {
-                Text("スコア").font(.title)
-                ForEach(scoreModel.scores.keys.sorted(), id: \.self) { key in
-                    Text("\(key.capitalized): \(scoreModel.scores[key]!, specifier: "%.1f")")
-                }
-
-                Text("おすすめの学習方法").font(.title).padding(.top)
-                ForEach(getMostNeededStudyMethods(scores: scoreModel.scores), id: \.self) { recommendation in
-                    Text(recommendation)
-                }
+        VStack(spacing: 20) {
+            Text("スコア").font(.title)
+            ForEach(scoreModel.scores.keys.sorted(), id: \.self) { key in
+                Text("\(key.capitalized): \(scoreModel.scores[key]!, specifier: "%.1f")")
+            }
+            Text("おすすめの学習方法").font(.title).padding(.top)
+            ForEach(getMostNeededStudyMethods(scores: scoreModel.scores), id: \.self) { recommendation in
+                Text(recommendation)
             }
         }
     }
