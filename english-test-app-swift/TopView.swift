2953bb0463d7863c8faca39265899281e6f9b231
KEN-4 <ssstututusss617@gmail.com>
2024-03-01 10:43:41 +0900

    Update TopView

--- english-test-app-swift/TopView.swift
+++ english-test-app-swift/TopView.swift
@@ -1,18 +1,30 @@
 import SwiftUI
 
 struct TopView: View {
+    // モーダル表示のトリガーとなる状態変数
+    @State private var showModal = false
+
     var body: some View {
         NavigationView {
             VStack {
-                NavigationLink(destination: QuestionView(viewModel: QuestionViewModel())) {
+                Button(action: {
+                    // ボタンをタップしたらモーダル表示をトリガー
+                    showModal = true
+                }) {
                     Text("診断開始")
                         .padding()
                 }
             }
             .navigationTitle("English Test")
+            // モーダル表示を定義
+            .fullScreenCover(isPresented: $showModal) {
+                // 表示するビューを指定
+                QuestionView(viewModel: QuestionViewModel())
+            }
         }
     }
 }
+
 
 struct Top_Previews: PreviewProvider {
     static var previews: some View {
