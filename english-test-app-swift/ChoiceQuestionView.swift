a2ec05533598e0144c32999ab9fd0636a4387089
KEN-4 <ssstututusss617@gmail.com>
2024-03-11 11:32:44 +0900

    Adapt submit button ChoieQuestionView

--- english-test-app-swift/ChoiceQuestionView.swift
+++ english-test-app-swift/ChoiceQuestionView.swift
@@ -4,38 +4,43 @@ import AVFoundation
 struct ChoiceQuestionView: View {
     @ObservedObject var viewModel: QuestionViewModel
     @State private var showingModal = false
-
+    
     var body: some View {
-        VStack {
-            if let question = viewModel.currentQuestion {
-                Text("正しいと思う選択肢を選んでください")
-                ForEach(question.sentences, id: \.self) { sentence in
-                    Text(sentence)
-                        .padding(.bottom, 8)
-                }
-                ForEach(question.choices, id: \.self) { choice in
-                    Button(choice) {
-                        viewModel.checkAnswer(choice: choice)
-                        viewModel.goToNextQuestion()
+        NavigationView{
+            VStack {
+                if let question = viewModel.currentQuestion {
+                    Text("正しいと思う選択肢を選んでください")
+                        .foregroundColor(.customBlack)
+                        .padding()
+                    // 選択肢を表示
+                    ForEach(question.choices, id: \.self) { choice in
+                        Button(action: {
+                            viewModel.selectedChoice = choice
+                        }) {
+                            Text(choice)
+                                .padding()
+                                .foregroundColor(viewModel.isAnswered ? Color.customGray : (viewModel.selectedChoice == choice ? Color.customBlack : Color.customWhite))
+                                .background(viewModel.isAnswered ? Color.customGray : (viewModel.selectedChoice == choice ? Color.customDarkBlue : Color.customBlue))
+                                .cornerRadius(10)
+                        }
+                        .disabled(viewModel.isAnswered)
                     }
-                    .disabled(viewModel.isAnswered)
+                } else {
+                    Text("質問をロード中...")
                 }
-                if viewModel.isAnswered {
-                    Text("正解: \(question.correctAnswer)")
-                    Button("次の質問") {
-                    }
-                }
-            } else {
-                Text("質問をロード中...")
+            }
+            .navigationTitle("選択問題")
+            // ResultViewへのモーダル遷移をviewModel.showResultViewに基づいて設定
+            .fullScreenCover(isPresented: $viewModel.showResultView) {
+                // ここでモーダルとして表示したいビューを指定
+                ResultView(scoreModel: viewModel.scoreModel)
             }
         }
-        // ResultViewへのモーダル遷移をviewModel.showResultViewに基づいて設定
-        .fullScreenCover(isPresented: $viewModel.showResultView) {
-            // ここでモーダルとして表示したいビューを指定
-            ResultView(scoreModel: viewModel.scoreModel)
-        }
-        .navigationTitle("Choice Question")
-        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
+struct ChoiceQuestionView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionView(viewModel: QuestionViewModel())
+    }
+}
