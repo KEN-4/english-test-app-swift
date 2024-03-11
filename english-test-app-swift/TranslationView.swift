61b56b44b9577cb45da0d7f5419baeac3f0d4793
KEN-4 <ssstututusss617@gmail.com>
2024-03-11 11:26:31 +0900

    Adapt submit button TranslationView

--- english-test-app-swift/TranslationView.swift
+++ english-test-app-swift/TranslationView.swift
@@ -10,13 +10,16 @@ struct TranslationView: View {
                 VStack {
                     if let currentQuestion = viewModel.currentQuestion {
                         Text("日本語の文を英文に訳してください")
-                            .font(.title)
+                            .foregroundColor(.customBlack)
                             .padding()
                         
                         // 会話文を表示
-                        ForEach(currentQuestion.sentences, id: \.self) { sentence in
-                            Text(sentence)
-                                .padding(.bottom, 8)
+                        if let sentences = viewModel.currentQuestion?.sentences {
+                            ForEach(sentences, id: \.self) { sentence in
+                                Text(sentence)
+                                    .foregroundColor(.customBlack)
+                                    .padding(.bottom, 8)
+                            }
                         }
                         
                         // 選択肢を表示
@@ -26,9 +29,8 @@ struct TranslationView: View {
                             }) {
                                 Text(choice)
                                     .padding()
-                                    .frame(maxWidth: .infinity)
-                                    .background(Color.blue)
-                                    .foregroundColor(.white)
+                                    .background(Color.customBlue)
+                                    .foregroundColor(.customWhite)
                                     .cornerRadius(8)
                             }
                         }
@@ -37,11 +39,11 @@ struct TranslationView: View {
                             viewModel.removeLastChoice()
                         }
                         .padding()
-                        .background(Color.red)
-                        .foregroundColor(.white)
+                        .background(Color.customRed)
+                        .foregroundColor(.customWhite)
                         .cornerRadius(8)
                         
-                        TextField("Add text by pressing the button", text: $viewModel.textInput)
+                        TextField("ボタンを押して文章を完成させてください", text: $viewModel.textInput)
                             .textFieldStyle(RoundedBorderTextFieldStyle())
                             .padding()
                         
@@ -49,38 +51,21 @@ struct TranslationView: View {
                             viewModel.clearChoices()
                         }
                         .padding()
-                        .background(Color.red)
-                        .foregroundColor(.white)
+                        .background(Color.customRed)
+                        .foregroundColor(.customWhite)
                         .cornerRadius(8)
-                        
-                        Button("答えをチェック") {
-                            viewModel.checkAnswers()
-                        }
-                        .padding()
-                        
-                        if viewModel.isAnswered {
-                            if let firstAnswer = viewModel.currentQuestion?.answers.first {
-                                Text("正解: \(firstAnswer)")
-                                    .padding()
-                            };                            Button(action: {
-                                viewModel.goToNextQuestion()
-                            }) {
-                                Text("次の質問へ")
-                                    .padding()
-                                    .frame(maxWidth: .infinity)
-                                    .background(Color.green)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(10)
-                            }
-                            .padding([.top, .bottom], 10)
-                        }
                     } else {
-                        ProgressView("質問を読み込み中...")
+                        Text("質問をロード中...")
                     }
                 }
-                .padding()
+                .navigationTitle("翻訳問題")
             }
-            .navigationBarTitle("Translation Test", displayMode: .inline)
         }
+    }
+}
+
+struct TranslationView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionView(viewModel: QuestionViewModel())
     }
 }
