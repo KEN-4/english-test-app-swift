ce02bc19443bfb6b88f837e3a9236f477cb15821
KEN-4 <ssstututusss617@gmail.com>
2024-03-13 10:03:36 +0900

    Update TopView add animation

--- english-test-app-swift/TopView.swift
+++ english-test-app-swift/TopView.swift
@@ -1,18 +1,36 @@
 import SwiftUI
 
 struct TopView: View {
-    // モーダル表示のトリガーとなる状態変数
     @State private var showModal = false
+    @State private var animateBackground = false
 
     var body: some View {
         NavigationView {
-            VStack {
-                Button(action: {
-                    showModal = true
-                }) {
-                    Text("診断開始").font(.title)
-                        .foregroundColor(.customBlue)
+            GeometryReader { geometry in
+                ZStack {
+                    LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.5), Color.purple.opacity(0.5)]),
+                                   startPoint: animateBackground ? .bottomLeading : .topTrailing,
+                                   endPoint: animateBackground ? .topTrailing : .bottomLeading)
+                        .animation(Animation.linear(duration: 3).repeatForever(autoreverses: true), value: animateBackground)
+                        .edgesIgnoringSafeArea(.all)
+
+                    VStack {
+                        Button(action: {
+                            showModal = true
+                        }) {
+                            Text("診断開始").font(.title)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.customBlue)
+                                .cornerRadius(10)
+                        }
+                        .frame(width: geometry.size.width * 0.8)
+                    }
+                    .frame(width: geometry.size.width, height: geometry.size.height)
                 }
+            }
+            .onAppear {
+                animateBackground = true
             }
             .navigationTitle("English Test")
             .fullScreenCover(isPresented: $showModal) {
@@ -22,7 +40,8 @@ struct TopView: View {
     }
 }
 
-
-#Preview {
-    TopView()
+struct TopView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopView()
+    }
 }
