e821a0392a8f8df51fe6924622cc0e68827d1a32
KEN-4 <ssstututusss617@gmail.com>
2024-04-07 17:53:22 +0900

    Change LaunchScreen Image

--- english-test-app-swift/LaunchScreen.swift
+++ english-test-app-swift/LaunchScreen.swift
@@ -14,7 +14,7 @@ struct LaunchScreen: View {
             }
         } else {
             ZStack {
-                Image("ant")
+                Image("icon")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 100, height: 100) // 初期サイズ
